9b02a3df279df2c6e0f8f19f1d723d8f13705ede
rkboyce <rdb20@pitt.edu>
2022-06-01 11:15:07 -0400

    untested WITH NULL AS null

--- reference_and_mapping_data/load_eu_drug_name_active_ingredient_table.sql
+++ reference_and_mapping_data/load_eu_drug_name_active_ingredient_table.sql
@@ -20,10 +20,11 @@ create table eu_drug_name_active_ingredient
 	-- details varchar
 );
 
--- eliminate any invalid utf8 characters using the following command
+-- eliminate any invalid utf8 characters using the following command   
 -- iconv -f utf-8 -t utf-8 -c EU_registered_drugs_by_active_ingredient.csv > EU_registered_drugs_by_active_ingredient_utf8.csv
 truncate eu_drug_name_active_ingredient;
-COPY eu_drug_name_active_ingredient FROM 'EU_registered_drugs_by_active_ingredient_utf8.csv' WITH DELIMITER E'\t' CSV HEADER QUOTE E'"';
+--COPY eu_drug_name_active_ingredient FROM 'EU_registered_drugs_by_active_ingredient_utf8.csv' WITH DELIMITER E'\t' CSV HEADER QUOTE E'"';
+COPY eu_drug_name_active_ingredient FROM 'EU_registered_drugs_by_active_ingredient_utf8.csv' WITH DELIMITER E'\t' CSV HEADER QUOTE E'"' NULL AS 'null'; --NULL AS 'null' has not be tested
 
 -- create the mapping table with the distinct EU drug names and active ingredient(s)
 drop table if exists eu_drug_name_active_ingredient_mapping;
