e23e1c970b7396dc9a6ca06ee669f7e686b0eca7
wolfderby <OllieRadburn@gmail.com>
2022-06-22 14:48:13 -0400

    data/faers or data/laers on s3 and local

--- s3_data_download.sh
+++ s3_data_download.sh
@@ -2,74 +2,95 @@
 #shell options #-s enable (set) each optname #globstar enables ** recursive dir search
 
 #uncomment source to debug from command line
-#source ../faers.config
+source ../faers.config
 
- #export AWS_PROFILE=user1
-
+#belt and suspenders aws credentials
 export "AWS_ACCESS_KEY_ID=${AWS_S3_ACCESS_KEY}"
-#echo "AWS_S3_ACCESS_KEY = ${AWS_S3_ACCESS_KEY}"
-
 export "AWS_SECRET_ACCESS_KEY=${AWS_S3_SECRET_KEY}"
-#echo "AWS_S3_BUCKET_NAME=${AWS_S3_BUCKET_NAME}"
-
 export "AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}"
 
+#output aws credentials
 aws configure list
 
-#exit;
-
-shopt -s globstar
+#set and echo globstar settings for ** used
+shopt -s globstar 
 shopt globstar
 cd ${BASE_FILE_DIR}
 
 #check if /data_from s3 does not exist then either create it or delete and recreate it
 if [ ! -d "data_from_s3" ]; then
-    echo data_from_s3 does not exist, will make - line 8
+    echo data_from_s3 does not exist, will make - line 22
 	mkdir data_from_s3
 else 
-    echo data_from_s3 does exist, will remove and recreate - line 11
+    echo data_from_s3 does exist, will remove and recreate - line 25
 	rm -r data_from_s3
 	mkdir data_from_s3
 fi
 cd data_from_s3
+
+#make use case for legacy data faers_or_laers
+if [ ${LOAD_NEW_YEAR} -le 2012 ]
+then
+    #echo we have laers data
+    faers_or_laers='laers';
+else
+    #echo we have faers data
+    faers_or_laers='faers';
+fi
+
 
 #if LOAD_NEW_DATA
 if [ "${LOAD_ALL_TIME}" = 1 ]; then
 
     #download s3://..data_test to local/data_from_s3
     echo data will be downloaded to ${BASE_FILE_DIR}/data_from_s3/
-    aws s3 cp s3://napdi-cem-sandbox-files/data/ ${BASE_FILE_DIR}/data_from_s3/ --recursive --exclude "*" --include "*.txt"
+    
+    #aws s3 cp s3://napdi-cem-sandbox-files/data_test/ ${BASE_FILE_DIR}/data_from_s3/ --recursive --exclude "*" --include "*.txt"
+    aws s3 cp s3://napdi-cem-sandbox-files/data_test/ ${BASE_FILE_DIR}/data_from_s3/ --recursive --exclude "*" --include "*.txt"
 
     #locally loop through domains and create one staged import file ("domain".txt ie demo.txt)
-    curdir=`pwd`;
+    data_from_s3_root_above_laers_faers=`pwd`;
     headempty=0
     echo 'pwd is ';
     echo `pwd` it should be path/to/data_from_s3;
-    for domain in demo drug indi outc reac rpsr ther; do
-        echo 'domain is '$domain;
-        #mkdir $domain #throws error because aws cp created it
-        cd $domain
-        #for name in "${BASE_FILE_DIR}"/data_from_s3/**/*.txt; do #if data_from_s3 isn't specified it might run /all_data/ and take forever
-        for name in ./**/*.txt; do
-        echo 'name is '$name;
-            if [ $headempty = 0 ]; then
-                #put header row into drug.txt
-                head -1 --quiet $name > $domain.txt
-                
-                headempty=1 &&
-                #tail outputs last -n+2 lines of a file #-q does not output the filename
-                tail -n +2 --quiet $name >> $domain.txt
-            else
-                #tail outputs last -n+2 lines of a file #-q does not output the filename
-                tail -n +2 --quiet $name >> $domain.txt
-            fi
+
+    #loop through faers and laers folders
+    for laers_faers in faers laers; do # indi outc reac rpsr ther; do
+    echo laers_faers is $laers_faers;
+    cd $laers_faers;
+    echo outer loop pwd is `pwd`
+
+        #loop through domains within faers or laers
+        for domain in demo drug; do # indi outc reac rpsr ther; do
+            echo 'domain is '$domain;
+            #mkdir $domain #throws error because aws cp created it
+            cd $domain
+            echo inner loop domain pwd is `pwd`
+            #for name in "${BASE_FILE_DIR}"/data_from_s3/**/*.txt; do #if data_from_s3 isn't specified it might run /all_data/ and take forever
+            for name in ./**/*.txt; do
+            echo 'name is '$name;
+                if [ $headempty = 0 ]; then
+                    #put header row into drug.txt
+                    head -1 --quiet $name > $domain.txt
+                    
+                    headempty=1 &&
+                    #tail outputs last -n+2 lines of a file #-q does not output the filename
+                    tail -n +2 --quiet $name >> $domain.txt
+                else
+                    #tail outputs last -n+2 lines of a file #-q does not output the filename
+                    tail -n +2 --quiet $name >> $domain.txt
+                fi
+                #break 60
+            done; #end for name in ./**/*.txt; do
+            #reset headempty for next domain
+            headempty=0
+            #returns you to the level above the domain folder
+            cd $data_from_s3_root_above_laers_faers/$laers_faers
             #break 60
-        done; #end for name in ./**/*.txt; do
-        #reset headempty for next domain
-        headempty=0
-        cd $curdir
-        #break 60
-    done; #end for domain in drug demo; do 
+        done; #end for domain in drug demo; do
+    echo just finished domain loop
+    cd $data_from_s3_root_above_laers_faers
+    done; #end for laers faers; do
 else #not LOAD_ALL_DATA equivalent to LOAD_NEW_DATA
     echo REPLACING ALL DATA IN data_from_s3 WITH DATA FROM ${LOAD_NEW_QUARTER}  ${LOAD_NEW_YEAR}
     
@@ -86,18 +107,8 @@ else #not LOAD_ALL_DATA equivalent to LOAD_NEW_DATA
         #echo 'pwd is ';
         #echo `pwd` it should be path/to/data_from_s3 aka BASE_FILE_DIR;
 
-        #make use case for legacy data faers_or_laers
-        if [ ${LOAD_NEW_YEAR} -le 2012 ]
-        then
-            #echo we have laers data
-            faers_or_laers='laers';
-        else
-            #echo we have faers data
-            faers_or_laers='faers';
-        fi
-
-        for domain in demo drug indi outc reac rpsr ther; do # indi rpsr outc; do
-            s3_bucket_source_path=s3://napdi-cem-sandbox-files/data/$domain/${LOAD_NEW_YEAR}/${LOAD_NEW_QUARTER}
+        for domain in demo drug; do #indi outc reac rpsr ther; do # indi rpsr outc; do
+            s3_bucket_source_path=s3://napdi-cem-sandbox-files/data_test/$domain/${LOAD_NEW_YEAR}/${LOAD_NEW_QUARTER}
             
 
             state=`aws s3 ls $s3_bucket_source_path`
@@ -113,7 +124,7 @@ else #not LOAD_ALL_DATA equivalent to LOAD_NEW_DATA
 
                     aws s3 cp $s3_bucket_source_path $data_from_s3_folder_path --exclude "*" --include "*$YYQQtxt" --recursive
                     #`pwd`
-                    cd "$domain"
+                    cd "$faers_or_laers/$domain"
 
                     #note ** requires shopt globstar
                     for name in ./**/*.txt; do       
