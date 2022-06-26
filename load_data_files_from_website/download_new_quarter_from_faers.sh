9e9473abd942673ba3cec6cc2745f9fb5b88a6f5
wolfderby <OllieRadburn@gmail.com>
2022-06-25 21:16:50 -0400

    download and upload new quarter data

---
+++ load_data_files_from_website/download_new_quarter_from_faers.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# this script downloads all the current ASCII format FAERS files from the FDA website
+# as of May 15th 2018
+#
+# bucknelius
+################################################################
+
+#source "../../faers.config" #for local testing
+source "${BASE_FILE_DIR}/faers.config"
+
+# 19q1| https://fis.fda.gov/content/Exports/faers_ascii_2019Q1.zip
+# 18a4| https://fis.fda.gov/content/Exports/faers_ascii_2018q4.zip
+# 18q3| https://fis.fda.gov/content/Exports/faers_ascii_2018q3.zip
+# 18q2| https://fis.fda.gov/content/Exports/faers_ascii_2018q2.zip
+# 18q1| https://fis.fda.gov/content/Exports/faers_ascii_2018q1.zip
+
+echo `pwd`
+cd "${BASE_FILE_DIR}"
+echo `pwd`
+
+rm -rf data_new
+mkdir data_new
+cd data_new
+
+# FAERS ASCII
+
+fileyearquarter="${LOAD_NEW_YEAR: -2}${LOAD_NEW_QUARTER}"
+zip_url=https://fis.fda.gov/content/Exports/faers_ascii_"${LOAD_NEW_YEAR}${LOAD_NEW_QUARTER}".zip
+echo will wget this $zip_url
+wget $zip_url
+unzip faers_ascii_${LOAD_NEW_YEAR}${LOAD_NEW_QUARTER}.zip  2>> error.txt 1>> output.txt
+mv ASCII/ASC_NTS.pdf ASCII/ASC_NTS"${fileyearquarter}".pdf
+
+
+echo `pwd`
+#mv FAQs.pdf ascii/FAQs"${fileyearquarter}".pdf
+#mv FAQs.doc ascii/FAQs"${fileyearquarter}".doc
+#mv Readme.pdf ascii/Readme"${fileyearquarter}.pdf"
+#mv Readme.doc ascii/Readme"${fileyearquarter}.doc"
+#mv ascii/ASC_NTS.doc ascii/ASC_NTS"${fileyearquarter}".doc
+
+# # cleanup from miscellaneous sub-directory names
+# mv ASCII/* ascii
+# mv asci/* ascii
+# mv asii/* ascii
+# rmdir ASCII
+# rmdir asci
+# rmdir asii
