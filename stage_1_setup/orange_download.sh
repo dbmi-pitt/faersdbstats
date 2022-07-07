7630df2ef4242103fa3f30057d229d71f7b9ac17
wolfderby <OllieRadburn@gmail.com>
2022-07-07 08:51:50 -0400

    cleaned up commented out code

--- stage_1_setup/orange_download.sh
+++ stage_1_setup/orange_download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-source ${FAERSDBSTATS_REPO_LOCATION}/faers.config
+source ${FAERSDBSTATS_REPO_LOCATION}/../faers.config
 
 export "AWS_ACCESS_KEY_ID=${AWS_S3_ACCESS_KEY}"
 #echo "AWS_S3_ACCESS_KEY = ${AWS_S3_ACCESS_KEY}"
@@ -15,16 +15,10 @@ cd ${BASE_FILE_DIR}
 
 aws configure list
 
-#maybe set ${JOB_START_TIME} to prepend log filenames
 
+# echo "Pwd is "
+# pwd
 
-#echo "Begin log "
-#echo ${Internal.Job.Name}
-
-echo "Pwd is "
-pwd
-
-#rm -rf ${CEM_DOWNLOAD_DATA_FOLDER}
 rm -rf ${BASE_FILE_DIR}/data
 
 echo CEM_ORANGE_BOOK_DOWNLOAD_URL is $CEM_ORANGE_BOOK_DOWNLOAD_URL
@@ -35,18 +29,11 @@ cd ${CEM_DOWNLOAD_DATA_FOLDER}_setup
 echo pwd is
 pwd
 
-
 curl -f ${CEM_ORANGE_BOOK_DOWNLOAD_URL} -JLO && echo "SUCCESS!" ||
     echo "It did not download double check your URL and that the file does not exist in pwd" |
 
-#works
-#curl -f 'https://www.fda.gov/media/76860/download' -JLO && echo "SUCCESS!" ||
-#    echo "It did not download double check your URL"
 
 filename=$(find . -maxdepth 1 -name "EOBZIP_*")
-
-#take off ./
-#filename=${filename:2}
 
 
 filename=${CEM_ORANGE_BOOK_DOWNLOAD_FILENAME}
@@ -73,13 +60,11 @@ else
 
     rm -rf orange-book-data-files
 
-    #mkdir -p "orange-book-data-files"/${CEM_DOWNLOAD_YEAR}/${CEM_DOWNLOAD_MONTH}
     mkdir -p "orange-book-data-files"
 
     echo BASE_FILE_DIR is
     echo ${BASE_FILE_DIR}
 
-    #unzip $filename -d "orange-book-data-files"/${CEM_DOWNLOAD_YEAR}/${CEM_DOWNLOAD_MONTH}
     unzip ${data_setup_path}/$filename -d "orange-book-data-files"
 
     echo "in ${data_setup_path}/orange-book-data-files line counts are as follows"
@@ -99,6 +84,3 @@ fi
 #exec >logfile.txt 2>&1
 
 #echo "End " ${Internal.Job.Name} " log " >> error.txt >> output.txt
-
-#echo "last minute aws credential check"
-#export AWS_S3_BUCKET_NAME="${AWS_S3_BUCKET_NAME}"
