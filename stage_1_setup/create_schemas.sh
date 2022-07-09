b6286d97c7dd2e0f2c258933dc369fdd811a6f65
wolfderby <OllieRadburn@gmail.com>
2022-07-09 14:19:55 -0400

    added compare w/ cem

--- stage_1_setup/create_schemas.sh
+++ stage_1_setup/create_schemas.sh
@@ -2,6 +2,6 @@
 log_location=${BASE_FILE_DIR}/logs/${LOG_FILENAME}
 source ${BASE_FILE_DIR}/faers_config.config
 echo ${DATABASE_PASSWORD}
-PGPASSWORD=$(echo ${DATABASE_PASSWORD}) psql -U ${DATABASE_USERNAME} -d ${DATABASE_NAME} -w --no-password -h ${DATABASE_HOST} -c "CREATE SCHEMA IF NOT EXISTS $(echo ${DATABASE_SCHEMA});"
+PGPASSWORD=$(echo ${DATABASE_PASSWORD}) psql -U ${DATABASE_USERNAME} -d ${DATABASE_NAME} -w --no-password -h ${DATABASE_HOST} -c "CREATE SCHEMA IF NOT EXISTS $(echo ${DATABASE_SCHEMA});" -o $log_location
 
-echo "CREATE_SCHEMAS TALKING" >> $log_location
+# echo "CREATE_SCHEMAS TALKING" >> $log_location
