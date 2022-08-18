9f095487b0548f2110e1b31c8e483918c78b961f
wolfderby <OllieRadburn@gmail.com>
2022-08-18 17:12:48 -0400

    better but failed running drug12Q3 ln215 unexpEOF

--- stage_2_initial_data/s3_data_download.sh
+++ stage_2_initial_data/s3_data_download.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+#uncomment source to debug from command line
+source ../../faers_config.config
+
 log_location=${BASE_FILE_DIR}/logs/${LOG_FILENAME}
 #shell options #-s enable (set) each optname #globstar enables ** recursive dir search
 
-#uncomment source to debug from command line
-#source ../faers_config.config
 printf '\n' >> $log_location
 echo "########### s3_data_download ######################" >> $log_location
 
@@ -22,10 +23,10 @@ cd ${BASE_FILE_DIR}
 
 #check if /data_from s3 does not exist then either create it or delete and recreate it
 if [ ! -d "data_from_s3" ]; then
-    echo data_from_s3 does not exist, will make - line 22
+    echo data_from_s3 does not exist, will make - line 26
 	mkdir data_from_s3
 else 
-    echo data_from_s3 does exist, will remove and recreate - line 25
+    echo data_from_s3 does exist, will remove and recreate - line 29
 	rm -r data_from_s3
 	mkdir data_from_s3
 fi
@@ -44,10 +45,11 @@ fi
 
 if [ "${LOAD_ALL_TIME}" = 1 ]; then
 
-    #download s3://..data_test to local/data_from_s3
+    #download s3://..data to local/data_from_s3
+    echo LOAD_ALL_TIME is 1
     echo data will be downloaded to ${BASE_FILE_DIR}/data_from_s3/
     
-    #aws s3 cp s3://napdi-cem-sandbox-files/data_test/ ${BASE_FILE_DIR}/data_from_s3/ --recursive --exclude "*" --include "*.txt"
+    #s3 download
     aws s3 cp s3://napdi-cem-sandbox-files/data/ ${BASE_FILE_DIR}/data_from_s3/ --recursive --exclude "*" --include "*.txt"
 
     #locally loop through domains and create one staged import file ("domain".txt ie demo.txt)
@@ -57,31 +59,81 @@ if [ "${LOAD_ALL_TIME}" = 1 ]; then
     echo `pwd` it should be path/to/data_from_s3;
 
     #loop through faers and laers folders
-    for laers_faers in faers laers; do # indi outc reac rpsr ther; do
+    for laers_faers in faers laers; do
     echo laers_faers is $laers_faers;
     cd $laers_faers;
     echo outer loop pwd is `pwd`
 
         #loop through domains within faers or laers
-        for domain in demo drug indi outc reac rpsr ther; do
+        for domain in demo drug; do # indi outc reac rpsr ther; do
             echo 'domain is '$domain;
             #mkdir $domain #throws error because aws cp created it
             cd $domain
             echo inner loop domain pwd is `pwd`
             #for name in "${BASE_FILE_DIR}"/data_from_s3/**/*.txt; do #if data_from_s3 isn't specified it might run /all_data/ and take forever
             for name in ./**/*.txt; do
-            echo 'name is '$name;
+            #substring expansion is thrown off by thefilename is 18Q1_new.txt name is ./2018/Q1/DEMO18Q1_new.txt
+            #to do make thefilename everything after the 3rd "/" #done was ${name: -12}
+            thefilename=${name: 10}
+            year='$'${name: 4:2}
+            qtr='$'${name: 7:2}
+            thefilenamedata='$'"${thefilename}""$qtr""$year"
+            echo "name is ${name}";
+            echo 'thefilename is '$thefilename;
+            echo 'thefilenamedata is '$thefilenamedata;
                 if [ $headempty = 0 ]; then
                     #put header row into drug.txt
-                    head -1 --quiet $name > $domain.txt
+                    echo 'head is empty'
+                   
+                   #build out header (1st line of txt file)
+                    header="$(head -1 --quiet ${name})"
+                    #remove \r
+                    header="${header%%[[:cntrl:]]}"
+                    #append column header $filename
+                    header+=\$filename
+                    #header+='\r'
+                    #add year and quarter
+                    header+=\$yr\$qtr
+                    #make integer field in db ^ < add to table creation ddls
                     
-                    headempty=1 &&
-                    #tail outputs last -n+2 lines of a file #-q does not output the filename
-                    tail -n +2 --quiet $name >> $domain.txt
+                    echo $header > ${domain}.txt
+                    
+                    echo "${domain}.txt" is the domain.txt
+                    
+                    headempty=1 &&                   
+                    
+                    #tail outputs last -n+2 lines of a file (shows all lines of report from the second line onwards) # --quiet does not output the filename                    
+                    tail -n +2 --quiet $name >> ${domain}.txt
+                    
                 else
-                    #tail outputs last -n+2 lines of a file #-q does not output the filename
+                    echo 'head is not empty'
+                    #tail outputs last -n+2 lines of a file (shows all lines of report from the second line onwards) # --quiet does not output the filename
+                    #output starting at line #2
                     tail -n +2 --quiet $name >> $domain.txt
                 fi
+                    #sed -zi 's|\r\n|\$\r\n|1g' ${domain}.txt
+                    echo thefilename is $thefilename
+                    echo thefilenamedata is $thefilenamedata
+
+                                        #change crlf to lf
+                    tr -d '\015' <${domain}.txt >${domain}_lf.txt
+                    # tr -d '\015' <${domain}.txt >${domain}.txt
+
+                    #add thefilenamedata to the end of every line in the file
+                    #sed "s|$|endoftheline|g" demo_lf.txt > demo_test.txt
+                    sed "s|$|$thefilenamedata|g" ${domain}_lf.txt > ${domain}.txt
+
+                    #replace first occurence of $thefilenamedata
+                    sed -i "0,/$thefilenamedata/{s/$thefilenamedata//}" ${domain}.txt
+                    
+                    #chop for _test ing
+                    #sed -i '11,$ d' ${domain}_test.txt
+                    #head ${domain}_test.txt #for testing
+                    #sed -zi "s|\$|${thefilenamedata}\r\n|1g" ${domain}.txt
+
+                    #sed -ziE "s|\r\n|\\\$${qtr}\r\n|1g" ${domain}.txt
+                    #sed -ziE "s|\r\n|\\\$${yr}\r\n|1g" ${domain}.txt
+                
                 #break 60
             done; #end for name in ./**/*.txt; do
             #reset headempty for next domain
@@ -97,7 +149,7 @@ else #not LOAD_ALL_DATA
     echo REPLACING ALL DATA IN data_from_s3 WITH DATA FROM ${LOAD_NEW_QUARTER}  ${LOAD_NEW_YEAR}
     echo "REPLACING ALL DATA IN data_from_s3 WITH DATA FROM" ${LOAD_NEW_QUARTER}  ${LOAD_NEW_YEAR} >> $log_location
     
-        #download s3://..data_test to local/data_from_s3
+        #download s3://..data to local/data_from_s3
         #echo "data will be downloaded to..."
         #echo ${BASE_FILE_DIR}/data_from_s3/
 
@@ -110,7 +162,7 @@ else #not LOAD_ALL_DATA
         #echo 'pwd is ';
         #echo `pwd` it should be path/to/data_from_s3 aka BASE_FILE_DIR;
 
-        for domain in demo drug indi outc reac rpsr ther; do # indi rpsr outc; do
+        for domain in demo drug; do # indi outc reac rpsr ther; do # indi rpsr outc; do
             s3_bucket_source_path=s3://napdi-cem-sandbox-files/data/$faers_or_laers/$domain/${LOAD_NEW_YEAR}/${LOAD_NEW_QUARTER}
             
 
@@ -134,16 +186,17 @@ else #not LOAD_ALL_DATA
                         #echo 'pwd is (in for loop)'
                         #echo `pwd`                        
                         #echo 'domain is '$domain                 
-                       #echo 'name is '
-                        echo "$name"
+                        echo "name is ${name}"
                             if [ $headempty = 0 ]; then
                                 #put header row into drug.txt
-                                head -1 --quiet $name > $domain.txt
+                                echo 'headempty'
+                                head -1 $name > $domain.txt
                                 
                                 headempty=1 &&
                                 #tail outputs last -n+2 lines of a file #-q does not output the filename
                                 tail -n +2 --quiet $name >> $domain.txt
                             else
+                                echo 'head not empty'
                                 #tail outputs last -n+2 lines of a file #-q does not output the filename
                                 tail -n +2 --quiet $name >> $domain.txt
                             fi #end if [ $headempty = 0 ]; then
@@ -156,7 +209,7 @@ else #not LOAD_ALL_DATA
         #echo '$curdir is '$curdir
         cd $curdir #not sure if needed
         #break 60
-    done; #end for domain in demo drug; do
+    done; #end for domain in /data/; do
     #done;
 fi #end if [ "${LOAD_ALL_TIME}" = 1 ]; then
 
