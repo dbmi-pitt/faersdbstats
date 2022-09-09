1afdc651431b7acce6e75c516a74ea94b5663419
wolfderby <OllieRadburn@gmail.com>
2022-09-09 14:33:18 -0400

    should fix FDA downloaded files 12Q4 thru 14Q2

--- troubleshooting_scripts/add_col_reac.sh
+++ troubleshooting_scripts/add_col_reac.sh
@@ -31,9 +31,13 @@ shopt -s globstar
 #for entire year 
 # for file in ./2014/**/*.txt; do
 
-for name in ./2012/**/*Q4.txt; do
+# for name in ./2012/**/*Q4.txt; do
+# for name in ./2013/**/*.txt; do
+# for name in ./2014/**/Q1.txt ./2014/**/Q2.txt; do
+for name in ./2012/**/*Q4.txt ./2013/**/*.txt ./2014/**/Q1.txt ./2014/**/Q2.txt; do
     echo 'name is' $name
     cp $name ${name:0:12}_bak.txt
+    echo 'just made '${name:0:12}_bak.txt
     #replace 5th $ with $$ put into *.txt (chopping off name's .txt w/ -4)
     # sed 's/\$/\$\$/5' $name > "${name::-4}".txt
 
@@ -46,11 +50,12 @@ for name in ./2012/**/*Q4.txt; do
    #           \      \
    # escaped $ n $$    replace 3rd one
    
-    sed 's/$/$\$/' <${name}_staged_with_lfs_only.txt > $name
+    #replace line breaks with $ to add an empty column of data for "$drugreac"
+    sed 's/$/\$/' <${name}_staged_with_lfs_only.txt > $name
 
  
     #build out header (1st line of txt file)
-    # header="$(head -1 --quiet "${name::-4}".txt)"
+    # header="$(head -1 --quiet "${name::-4}".txt)"REAC14Q3
     
     #remove \r
     # header="${header%%[[:cntrl:]]}"
@@ -58,14 +63,15 @@ for name in ./2012/**/*Q4.txt; do
     #headers we want
     #primaryid$caseid$drug_seq$role_cod$drugname$prod_ai$val_vbm$route$dose_vbm$cum_dose_chr$cum_dose_unit$dechal$rechal$lot_num$exp_dt$nda_num$dose_amt$dose_unit$dose_form$dose_freq
 
-    # sed -i 's/\$pt\$/\$pt\$drug_rec_act/g' $name
-      sed -i '0,/\$pt\$/{s/\$pt\$/\$drug_rec_act\$/}' reac.txt
+    # replace only one occurence of $pt$ with $pt$drug_rec_act
+      sed -i '0,/\$pt\$\$/{s/\$pt\$/\$pt\$drug_rec_act/}' $name
 
     #make a sm.txt file that has only 20 lines to check results
     sed '20,$ d' $name > "${name:0:10}"sm.txt
 
-    head ../../reac.txt
-    head REAC12Q4.txt
+    head $name
+    # head ./REAC12Q4.txt
+    echo `pwd`
 
     echo '... if results are what you are after comment out the aws s3 download and/or cp and/or sync and re-run ./s3_data_download.sh to rebuilt reac.txt'
     echo 'you might also like to limit the for domain loop to just reac'
