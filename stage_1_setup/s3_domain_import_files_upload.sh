7f07ee16c63a3068b83e9efde03c2426c27d0c31
wolfderby <OllieRadburn@gmail.com>
2022-10-10 11:43:54 -0400

    stg 1 now has REBUILD_DATA=1 then s3 data upload

--- stage_1_setup/s3_domain_import_files_upload.sh
+++ stage_1_setup/s3_domain_import_files_upload.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+#if testing or debugging it is recommended to toggle the laers_faers loop to just one and the "for domain" loop for just one domain
+
 source ../../faers_config.config
 
 cd ${BASE_FILE_DIR}/data_from_s3/
 
-# for laers_faers in laers faers; do
-# for laers_faers in laers; do
-for laers_faers in faers; do
+for laers_faers in laers faers; do
+# for laers_faers in faers; do #for testing
+# for laers_faers in laers; do #after testing
 
-    # for domain in demo; do
+    # for domain in demo; do #for testing
     for domain in demo drug indi outc reac rpsr ther; do
-    # for domain in drug indi outc reac rpsr ther; do
+    # for domain in drug indi outc reac rpsr ther; do #after testing
 
         echo pwd is `pwd`
         file_path=${BASE_FILE_DIR}'/data_from_s3/'$laers_faers'/'$domain'/'${domain}'.txt'
@@ -20,6 +22,5 @@ for laers_faers in faers; do
         aws s3 cp $file_path $dest_path
 
     done; #done domain
-
 
 done; #done laers_faers
