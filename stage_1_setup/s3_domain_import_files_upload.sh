cd649c49e99bbe18d9c69e7c3486dab69289de19
wolfderby <OllieRadburn@gmail.com>
2022-10-12 14:37:33 -0400

    upload to $AWS_S3_BUCKET_NAME/data

--- stage_1_setup/s3_domain_import_files_upload.sh
+++ stage_1_setup/s3_domain_import_files_upload.sh
@@ -16,7 +16,7 @@ for laers_faers in laers faers; do
 
         echo pwd is `pwd`
         file_path=${BASE_FILE_DIR}'/data_from_s3/'$laers_faers'/'$domain'/'${domain}'.txt'
-        dest_path='s3://'${AWS_S3_BUCKET_NAME}'/'${AWS_S3_DOMAIN_BACKUP_DEST}'/'$laers_faers'/'$domain'/'${domain}'.txt'
+        dest_path='s3://'${AWS_S3_BUCKET_NAME}'/data/'$laers_faers'/'$domain'/'${domain}'.txt'
         echo 'running aws s3 cp '$file_path $dest_path
         # aws s3 cp --follow-symlinks --recursive ./$domain/${domain}.txt s3://${AWS_S3_BUCKET_NAME}/${AWS_S3_DOMAIN_BACKUP_DEST}/$laers_faers/$domain/${domain}.txt
         aws s3 cp $file_path $dest_path
