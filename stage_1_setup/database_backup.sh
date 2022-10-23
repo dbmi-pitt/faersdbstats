70f912145bdcba286b7b6fc4ad48fb21be064ec1
wolfderby <OllieRadburn@gmail.com>
2022-10-23 14:40:15 -0400

    added DATABASE_NAME to dump filename

--- stage_1_setup/database_backup.sh
+++ stage_1_setup/database_backup.sh
@@ -1,5 +1,5 @@
 run_date=$(date "+%m%d%Y")
-DB_BACKUP_FILENAME="${LOG_FILENAME_ROOT}_stg_1_auto_bk_fcd_${run_date}.dump"
+DB_BACKUP_FILENAME="${DATABASE_NAME}_${LOG_FILENAME_ROOT}_stg_1_auto_bk_fcd_${run_date}.dump"
 DB_BACKUP_FILENAME_FCD="${LOG_FILENAME_ROOT}_stg_1_auto_bk_fcd_${run_date}.dump"
 DB_BACKUP_FILENAME_PATH=${DATABASE_BACKUP_LOCATION}${DB_BACKUP_FILENAME}
 DB_BACKUP_FILENAME_PATH_FCD=${DATABASE_BACKUP_LOCATION}${DB_BACKUP_FILENAME_FCD}
@@ -9,7 +9,7 @@ echo '${DB_BACKUP_FILENAME} is ' ${DB_BACKUP_FILENAME}
 echo ${DB_BACKUP_FILENAME_PATH}
 
 # creates larger database back-up file
-PGPASSWORD="${DATABASE_PASSWORD}" pg_dump -h ${DATABASE_HOST} -p ${DATABASE_PORT} -U ${DATABASE_USERNAME} ${DATABASE_NAME} > ${DB_BACKUP_FILENAME_PATH}
+# PGPASSWORD="${DATABASE_PASSWORD}" pg_dump -h ${DATABASE_HOST} -p ${DATABASE_PORT} -U ${DATABASE_USERNAME} ${DATABASE_NAME} > ${DB_BACKUP_FILENAME_PATH}
 
 
 #note -Fc = Format custom Output a custom-format archive suitable for input into
