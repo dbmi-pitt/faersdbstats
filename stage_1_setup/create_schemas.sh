2fb002f2ce44aadb4a5063a4a529dd7da8a5e5a7
wolfderby <OllieRadburn@gmail.com>
2022-10-24 14:39:24 -0400

    omop job added

--- stage_1_setup/create_schemas.sh
+++ stage_1_setup/create_schemas.sh
@@ -3,5 +3,4 @@ log_location=${BASE_FILE_DIR}/logs/create_schemas_sh.txt
 source ${BASE_FILE_DIR}/faers_config.config
 echo ${DATABASE_PASSWORD}
 PGPASSWORD=$(echo ${DATABASE_PASSWORD}) psql -U ${DATABASE_USERNAME} -d ${DATABASE_NAME} -w --no-password -h ${DATABASE_HOST} -c "CREATE SCHEMA IF NOT EXISTS $(echo ${DATABASE_SCHEMA});" -o $log_location
-
-# echo "CREATE_SCHEMAS TALKING" >> $log_location
+PGPASSWORD=$(echo ${DATABASE_PASSWORD}) psql -U ${DATABASE_USERNAME} -d ${DATABASE_NAME} -w --no-password -h ${DATABASE_HOST} -c "CREATE SCHEMA IF NOT EXISTS $(echo staging_vocabulary);" -o $log_location
