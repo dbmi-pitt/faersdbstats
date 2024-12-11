1f40e9aadd4ce17606e69ca8bacaba1b12df45f6
wolfderby <OllieRadburn@gmail.com>
2024-12-11 16:51:00 -0500

    big revision coming

--- stage_1_setup/data_from_fda_scripts/download_faers_fda_data_fix.sh
+++ stage_1_setup/data_from_fda_scripts/download_faers_fda_data_fix.sh
@@ -1,52 +1,35 @@
 #!/bin/bash
-# /home/pentaho-secondary/projects-brb265-2024/faersdbstats/faersdbstats/stage_1_setup/data_from_fda_scripts/download_faers_fda_data_fix.sh
-#uncomment source to debug from command line
+# /home/pentaho-secondary/projects-brb265-2024/faersdbstats/faersdbstats/stage_1_setup/faersdbstats_data_test_scripts/download_faers_fda_data_fix.sh
+
+# Uncomment source to debug from command line
 # source ../faers_config.config
 source ../../../faers_config.config
 
 # cat ../../../faers_config.config
 
-echo "/n"
-echo "BASE_FILE_DIR is :$BASE_FILE_DIR"
+# echo "/n"
+echo "DATABASE_BACKUP_LOCATION is :$DATABASE_BACKUP_LOCATION"
 
-
-#from download_new_quarter_from_fda
-# faers_or_laers='laers';
-# fileyearquarter="${LOAD_NEW_YEAR: -2}${LOAD_NEW_QUARTER,,}"
-#  zip_url=https://fis.fda.gov/content/Exports/aers_ascii_"${LOAD_NEW_YEAR}${LOAD_NEW_QUARTER,,}".zip
-# zip_filename="aers_ascii_${LOAD_NEW_YEAR}${LOAD_NEW_QUARTER,,}.zip"
-# echo will wget this $zip_url
-# wget $zip_url 2>&1
-# unzip $zip_filename 2>> error.txt 1>> output.txt
-# mv ASCII/ASC_NTS.pdf ASCII/ASC_NTS"${fileyearquarter}".pdf
-#set and echo globstar settings for ** used
+# Set and echo globstar settings for ** used
 shopt -s globstar 
 shopt globstar
-    if [ ! -d ${BASE_FILE_DIR}/data_from_fda ]; then
-        mkdir ${BASE_FILE_DIR}/data_from_fda
+    if [ ! -d ${DATABASE_BACKUP_LOCATION}/faersdbstats_data_test ]; then
+        mkdir -p ${DATABASE_BACKUP_LOCATION}/faersdbstats_data_test
     fi
-    cd ${BASE_FILE_DIR}/data_from_fda/
-    dir_above_laers_or_faers=`pwd`;
-exit;
-quit;
+    cd ${DATABASE_BACKUP_LOCATION}/faersdbstats_data_test/
+    dir_above_laers_or_faers=$(pwd)
 # for laers_faers in laers faers; do 
-for laers_faers in faers; do # faers; do 
-    if [ ! -d ${BASE_FILE_DIR}/data_from_s3/$laers_faers ]; then
-        mkdir ${BASE_FILE_DIR}/data_from_s3/$laers_faers
-    fi
-    cd ${BASE_FILE_DIR}/data_from_s3/$laers_faers
-    # for domain in demo drug indi; do #outc reac rpsr ther; do
-    for domain in demo drug indi outc reac rpsr ther; do
-    # for domain in outc reac rpsr ther; do
-        if [ ! -d $domain ]; then
-            mkdir $domain
-        fi
-        cd $domain
+for laers_faers in faers; do 
+    mkdir -p $dir_above_laers_or_faers/$laers_faers
+
+    cd $dir_above_laers_or_faers/$laers_faers
+
+
         # for THIS_YEAR in 2004 2005 2006 2007 2008 2009 2010 2011 2012; do
         for THIS_YEAR in 2022; do
             # for THIS_QUARTER in Q1 Q2 Q3 Q4; do
-            for THIS_QUARTER in Q4; do
-                echo $laers_faers - $domain - ${THIS_YEAR} - ${THIS_QUARTER}
+            for THIS_QUARTER in Q3; do
+                echo $laers_faers - ${THIS_YEAR} - ${THIS_QUARTER}
                 LOAD_NEW_YEAR=$THIS_YEAR
                 LOAD_NEW_QUARTER=$THIS_QUARTER
                 qtr_num=${LOAD_NEW_QUARTER: 1}
@@ -72,37 +55,43 @@ for laers_faers in faers; do # faers; do
                 # echo ${qtr_num}
                 # echo `pwd`
                 # zip_url=https://fis.fda.gov/content/Exports/aers_ascii_2005q1.zip
-                echo 'https://fis.fda.gov/content/Exports/aers_ascii_'${LOAD_NEW_YEAR}'q'${THIS_QUARTER_num}'.zip'
-                zip_url='https://fis.fda.gov/content/Exports/aers_ascii_'${LOAD_NEW_YEAR}'q'${THIS_QUARTER_num}'.zip'
+                echo 'https://fis.fda.gov/content/Exports/faers_ascii_'${LOAD_NEW_YEAR}'q'${THIS_QUARTER_num}'.zip'
+                zip_url='https://fis.fda.gov/content/Exports/faers_ascii_'${LOAD_NEW_YEAR}'q'${THIS_QUARTER_num}'.zip'
                 echo will wget this $zip_url
-                #download data -P = to specific dir data_from_fda
+                #download data -P = to specific dir faersdbstats_data_test
                 #skip 12 Q4
-                if [ $THIS_YEAR !== '2012' ] && [ $THIS_QUARTER !== 'Q4' ]; then
-                   wget $zip_url --no-clobber -P ${dir_above_laers_or_faers}/data_from_fda 2>&1
-                else 
-                    echo 'not 12 Q4'
-                fi
+                # if [ "$THIS_YEAR" != '2012' ] && [ "$THIS_QUARTER" != 'Q4' ]; then
+                    wget $zip_url --no-clobber -P ${dir_above_laers_or_faers}/faersdbstats_data_test 2>&1
+                # else 
+                #     echo 'Skipping 2012 Q4 as instructed'
+                # fi
                 # if [ ! -d ascii ]; then
                 #   mkdir ascii
                 # fi
-                if [ ! -d $dir_above_laers_or_faers/data_from_fda/$laers_faers/$domain/${THIS_YEAR}/${THIS_QUARTER} ]; then
-                    mkdir -p $dir_above_laers_or_faers/data_from_fda/$laers_faers/$domain/${THIS_YEAR}/${THIS_QUARTER}
+                if [ ! -d $dir_above_laers_or_faers/$laers_faers/$domain/${THIS_YEAR}/${THIS_QUARTER} ]; then
+                    mkdir -p $dir_above_laers_or_faers/$laers_faers/$domain/${THIS_YEAR}/${THIS_QUARTER}
                 fi
-                zip_filename_loc=$dir_above_laers_or_faers/data_from_fda/'aers_ascii_'${THIS_YEAR}'q'${qtr_num}'.zip'
+                zip_filename_loc=$dir_above_laers_or_faers/'faers_ascii_'${THIS_YEAR}'q'${qtr_num}'.zip'
                 #unzip -n = no overwrite
-                unzip -n $zip_filename_loc -d $dir_above_laers_or_faers/data_from_fda/$laers_faers/$domain/${THIS_YEAR}/${THIS_QUARTER} 2>> error.txt 1>> output.txt
-                if [ -f $dir_above_laers_or_faers/data_from_fda/$laers_faers/$domain/${THIS_YEAR}/${THIS_QUARTER}/ascii/${domain^^}${THIS_YEAR:2}${THIS_QUARTER}.TXT ]; then
-                    mv  $dir_above_laers_or_faers/data_from_fda/$laers_faers/$domain/${THIS_YEAR}/${THIS_QUARTER}/ascii/${domain^^}${THIS_YEAR:2}${THIS_QUARTER}.TXT $dir_above_laers_or_faers/$laers_faers/$domain/${THIS_YEAR}/${THIS_QUARTER}/${domain^^}${THIS_YEAR:2}${THIS_QUARTER}.txt
+                unzip -n $zip_filename_loc -d $dir_above_laers_or_faers/$laers_faers/$domain/${THIS_YEAR}/${THIS_QUARTER} 2>> error.txt 1>> output.txt
+                if [ -f $dir_above_laers_or_faers/$laers_faers/$domain/${THIS_YEAR}/${THIS_QUARTER}/ascii/${domain^^}${THIS_YEAR:2}${THIS_QUARTER}.TXT ]; then
+                    mv  $dir_above_laers_or_faers/$laers_faers/$domain/${THIS_YEAR}/${THIS_QUARTER}/ascii/${domain^^}${THIS_YEAR:2}${THIS_QUARTER}.TXT $dir_above_laers_or_faers/$laers_faers/$domain/${THIS_YEAR}/${THIS_QUARTER}/${domain^^}${THIS_YEAR:2}${THIS_QUARTER}.txt
                 fi
-                # if [ -f $dir_above_laers_or_faers/data_from_fda/$laers_faers/$domain/${THIS_YEAR}/${THIS_QUARTER}/ascii/${domain^^}${THIS_YEAR:2}Q${THIS_QUARTER}.txt ]; then
-                #     mv  $dir_above_laers_or_faers/data_from_fda/$laers_faers/$domain/${THIS_YEAR}/${THIS_QUARTER}/ascii/${domain^^}${THIS_YEAR:2}Q${THIS_QUARTER}.txt $dir_above_laers_or_faers/$laers_faers/$domain/${THIS_YEAR}/Q${THIS_QUARTER}/${domain^^}${THIS_YEAR:2}${THIS_QUARTER}.txt
+    # for domain in demo drug indi; do #outc reac rpsr ther; do
+    for domain in demo drug indi outc reac rpsr ther; do
+    # for domain in rpsr; do
+        mkdir -p $dir_above_laers_or_faers/$laers_faers/$domain
+
+        cd $dir_above_laers_or_faers/$laers_faers/$domain
+                # if [ -f $dir_above_laers_or_faers/$laers_faers/$domain/${THIS_YEAR}/${THIS_QUARTER}/ascii/${domain^^}${THIS_YEAR:2}Q${THIS_QUARTER}.txt ]; then
+                #     mv  $dir_above_laers_or_faers/$laers_faers/$domain/${THIS_YEAR}/${THIS_QUARTER}/ascii/${domain^^}${THIS_YEAR:2}Q${THIS_QUARTER}.txt $dir_above_laers_or_faers/$laers_faers/$domain/${THIS_YEAR}/Q${THIS_QUARTER}/${domain^^}${THIS_YEAR:2}${THIS_QUARTER}.txt
                 # fi
-                # if [ -f ${dir_above_laers_or_faers}/data_from_fda/ascii/DEMO${LOAD_NEW_YEAR}q${LOAD_NEW_QUARTER}.txt ]; then
-                #     mv ${dir_above_laers_or_faers}/data_from_fda/ascii/DEMO${LOAD_NEW_YEAR:2}${LOAD_NEW_QUARTER}.txt ascii/${LOAD_NEW_YEAR}/${LOAD_NEW_QUARTER}/DEMO${LOAD_NEW_YEAR:2}${LOAD_NEW_QUARTER}.txt
+                # if [ -f ${dir_above_laers_or_faers}/faersdbstats_data_test/ascii/DEMO${LOAD_NEW_YEAR}q${LOAD_NEW_QUARTER}.txt ]; then
+                #     mv ${dir_above_laers_or_faers}/faersdbstats_data_test/ascii/DEMO${LOAD_NEW_YEAR:2}${LOAD_NEW_QUARTER}.txt ascii/${LOAD_NEW_YEAR}/${LOAD_NEW_QUARTER}/DEMO${LOAD_NEW_YEAR:2}${LOAD_NEW_QUARTER}.txt
                 # fi
-                # echo 'about to check if this exists - ' ${dir_above_laers_or_faers}/data_from_fda/ASCII/ASC_NTS.pdf ascii/${THIS_YEAR}/${THIS_QUARTER}/ASC_NTS${fileyearquarter}.pdf
-                if [ -f ${dir_above_laers_or_faers}/data_from_fda/ASCII/ASC_NTS.pdf ]; then
-                    mv ${dir_above_laers_or_faers}/data_from_fda/ASCII/ASC_NTS.pdf ascii/${THIS_YEAR}/${LOAD_NEW_QUARTER}/ASC_NTS${fileyearquarter}.pdf
+                # echo 'about to check if this exists - ' ${dir_above_laers_or_faers}/faersdbstats_data_test/ASCII/ASC_NTS.pdf ascii/${THIS_YEAR}/${THIS_QUARTER}/ASC_NTS${fileyearquarter}.pdf
+                if [ -f ${dir_above_laers_or_faers}/faersdbstats_data_test/ASCII/ASC_NTS.pdf ]; then
+                    mv ${dir_above_laers_or_faers}/faersdbstats_data_test/ASCII/ASC_NTS.pdf ascii/${THIS_YEAR}/${LOAD_NEW_QUARTER}/ASC_NTS${fileyearquarter}.pdf
                 fi
             done; #end quarter loop
         cd $dir_above_laers_or_faers/$laers_faers/$domain
