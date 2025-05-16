d9ae16a958eb64529ab827dd65ae071af387cccb
wolfderby <OllieRadburn@gmail.com>
2025-05-16 14:25:05 -0400

    bring in load_drug_usagi_mapping.sql since we've updated usagi

--- stage_5_sqls/load_drug_usagi_mapping.sql
+++ stage_5_sqls/load_drug_usagi_mapping.sql
@@ -6,8 +6,10 @@
 --
 -- LTS COMPUTING LLC
 ------------------------------
+set search_path = ${DATABASE_SCHEMA};
+
 insert into drug_usagi_mapping
-select a.source_code_description as drug_name_original , 
+select a.source_code_description as drug_name_original ,
 b.concept_name, b.concept_class_id, cast(a.target_concept_id as integer) as concept_id, cast ('usagi' as text) as update_method
 from usagi_import a
 inner join staging_vocabulary.concept b
